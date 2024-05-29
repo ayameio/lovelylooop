b22f544b4d2e7b00c26b4398116a1b2b0d24d1c4
Supermalina <adriangaspar@protonmail.com>
2024-05-29 19:03:42 +0200

    cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,21 +21,17 @@ export default function Home() {
 				width="1920"
 			/>
 			<div className="relative z-10 flex flex-col items-center justify-center h-full px-4 md:px-6 lg:px-8 gap-4">
-				<div className="grid grid-cols-3 justify-center gap-4 w-full max-w-lg">
-					<div></div>
-					<Button className="text-md shadow-[0_0_20px_rgba(0,0,0,0.5)] bg-white/50 text-black hover:bg-white transition-colors duration-300 h-12 rounded-xl"><a href="/about" target="_blank" className="text-sm">About</a></Button>
-					<div></div>
-				</div>
 				<div className="grid grid-cols-3 gap-4 w-full max-w-lg">
 					<Button className="shadow-[0_0_20px_rgba(0,0,0,0.5)] bg-white/50 text-black hover:bg-white transition-colors duration-300 h-12 rounded-xl">Wallpapers</Button>
 					<Button className="shadow-[0_0_20px_rgba(0,0,0,0.5)] bg-white/50 text-black hover:bg-white transition-colors duration-300 h-12 rounded-xl">Collections</Button>
 					<Button className="shadow-[0_0_20px_rgba(0,0,0,0.5)] bg-white/50 text-black hover:bg-white transition-colors duration-300 h-12 rounded-xl">Token</Button>
 				</div>
-				<div className="grid grid-cols-2 gap-4 w-full max-w-lg">
+				<div className="grid grid-cols-3 gap-4 w-full max-w-lg">
+				<Button className="text-md shadow-[0_0_20px_rgba(0,0,0,0.5)] bg-white/50 text-black hover:bg-white transition-colors duration-300 h-12 rounded-xl"><a href="/about" target="_blank" className="text-sm">About</a></Button>
 					<Button className="font-bold text-md shadow-[0_0_20px_rgba(0,0,0,0.5)] bg-white/50 text-black hover:bg-white transition-colors duration-300 h-12 rounded-xl"><a href="https://t.me/lovelylooopBot" target="_blank">@lovelylooopBot</a></Button>
 					<Button className="font-bold text-md shadow-[0_0_20px_rgba(0,0,0,0.5)] bg-white/50 text-black hover:bg-white transition-colors duration-300 h-12 rounded-xl">Mini-Games</Button>
 				</div>
-				<p className="mt-6 h-6 rounded-xl bg-white font-bold text-center text-lg text-black flex flex-row justify-center items-center text-sm p-4 shadow-[0_0_20px_rgba(0,0,0,0.5)] bg-black/50 text-white">$LYL Token Address</p>
+				<p className="mt-6 h-6 rounded-xl bg-white font-bold text-center text-lg text-black flex flex-row justify-center items-center text-sm p-4 shadow-[0_0_20px_rgba(0,0,0,0.5)] bg-black/50 text-white">Token Address</p>
 				<div className="h-12 rounded-xl w-full max-w-lg shadow-[0_0_20px_rgba(0,0,0,0.5)] bg-black/50 text-white flex flex-col justify-center">
 					<p className="text-sm text-center">EQDSqgy_LJUD9EabMyf4Gdjw1GHWh_cKLcdbOZ3wCCUvoZFx</p>
 				</div>
