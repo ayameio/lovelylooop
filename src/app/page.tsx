9a342293ba6661d8136b7bb56e010c95350350b4
Supermalina <adriangaspar@protonmail.com>
2024-05-29 18:57:17 +0200

    fixed token address container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,9 +35,7 @@ export default function Home() {
 					<Button className="font-bold text-md shadow-[0_0_20px_rgba(0,0,0,0.5)] bg-white/50 text-black hover:bg-white transition-colors duration-300 h-12 rounded-xl"><a href="https://t.me/lovelylooopBot" target="_blank">@lovelylooopBot</a></Button>
 					<Button className="font-bold text-md shadow-[0_0_20px_rgba(0,0,0,0.5)] bg-white/50 text-black hover:bg-white transition-colors duration-300 h-12 rounded-xl">Mini-Games</Button>
 				</div>
-				<div className="mt-6 h-6 rounded-xl bg-white font-bold text-center text-lg text-black flex flex-row justify-center items-center text-sm p-4 shadow-[0_0_20px_rgba(0,0,0,0.5)] bg-black/50 text-white">
-					<p>[$LYL] Token Address</p>
-				</div>
+				<p className="mt-6 h-6 rounded-xl bg-white font-bold text-center text-lg text-black flex flex-row justify-center items-center text-sm p-4 shadow-[0_0_20px_rgba(0,0,0,0.5)] bg-black/50 text-white">[$LYL] Token Address</p>
 				<div className="h-12 rounded-xl w-full max-w-lg shadow-[0_0_20px_rgba(0,0,0,0.5)] bg-black/50 text-white flex flex-col justify-center">
 					<p className="text-sm text-center">EQDSqgy_LJUD9EabMyf4Gdjw1GHWh_cKLcdbOZ3wCCUvoZFx</p>
 				</div>
